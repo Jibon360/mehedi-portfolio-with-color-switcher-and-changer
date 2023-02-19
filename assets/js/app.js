bb3e1ee86ed55da49b4f53b6320d23f4795ce7a2
Mehedi <mjjibon115@gmail.com>
2023-02-19 20:16:34 +0600

    client section end

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,4 +25,23 @@ $(() => {
     });
 
 
+    // client/testimonial owl carousel
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
 })
