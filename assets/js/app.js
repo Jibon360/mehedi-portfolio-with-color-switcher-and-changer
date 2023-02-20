022bec8034fd28c098e39933b79be010c80897e1
Mehedi <mjjibon115@gmail.com>
2023-02-20 13:06:19 +0600

    navbar and banner responsive fiex

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,21 +27,53 @@ $(() => {
 
     // client/testimonial owl carousel
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:false,
-        autoplay:true,
-        responsive:{
-            0:{
-                items:1
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1
             },
-            600:{
-                items:1
+            600: {
+                items: 1
             },
-            1000:{
-                items:1
+            1000: {
+                items: 1
             }
         }
     })
 
+
+    // color switcher
+
+    $(".color-switchericon").on('click', function () {
+        $(".color-swithcer").toggleClass("themeswithcershow");
+    })
+
+
+    $(".colorbtn").each(function (param) {
+        $(this).on('click', function (param) {
+            let colors = $(this).attr('data-color');
+            document.documentElement.style.setProperty('--theme-color', colors);
+        })
+    })
+
+
+
+    $(".linebox").on('click',function(){
+       $(".line").toggleClass("closebox");
+    })
+
+
+
+
+
 })
+
+
+
+
+function myFunction(x) {
+    x.classList.toggle("change");
+  }
